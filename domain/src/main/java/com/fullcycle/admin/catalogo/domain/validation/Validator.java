80cb69cc92a83bdcec2c1c0a0dd3696907b6d1bd
Highlander Dantas <highlander.dantas@sankhya.com.br>
2023-08-27 20:20:37 -0300

    modulo java fullcycle

---
+++ domain/src/main/java/com/fullcycle/admin/catalogo/domain/validation/Validator.java
@@ -0,0 +1,14 @@
+package com.fullcycle.admin.catalogo.domain.validation;
+
+public abstract class Validator {
+    private final ValidationHandler handler;
+    protected Validator(final ValidationHandler aHandler) {
+        this.handler = aHandler;
+    }
+
+    protected ValidationHandler validationHandler() {
+        return handler;
+    }
+
+    public abstract void validate();
+}
